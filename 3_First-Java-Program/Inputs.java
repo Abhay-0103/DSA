e9461195da3af0557cd92e7ea11a646525908d2c
Abhay-0103 <ab0321054@gmail.com>
2025-08-07 00:33:09 +0530

    input variables

---
+++ 3_First-Java-Program/Inputs.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("please enter one input: ");
+//        int rollno = input.nextInt();
+//        System.out.println("your roll number is " + rollno);
+//
+
+
+
+    }
+}
