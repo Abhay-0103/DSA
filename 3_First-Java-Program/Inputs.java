db310a3b3685484011e1abac32aee26188f7be3d
Abhay-0103 <ab0321054@gmail.com>
2025-08-08 19:05:28 +0530

    Sum

--- 3_First-Java-Program/Inputs.java
+++ 3_First-Java-Program/Inputs.java
@@ -7,8 +7,18 @@ public class Inputs {
 //        int rollno = input.nextInt();
 //        System.out.println("your roll number is " + rollno);
 //
+            int a = 234_000_000;
+        System.out.println(a);
 
+        // other example
+        System.out.println("enter your name : ");
+        String name = input.nextLine();
+        System.out.println(name);
 
+        // example for float
+        System.out.println("Input the makes ");
+        float marks = input.nextFloat();
+        System.out.println(marks);
 
     }
 }
