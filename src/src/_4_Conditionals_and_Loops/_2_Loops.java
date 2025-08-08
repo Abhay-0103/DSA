3b7592301b617004d7e5a29c7e82186f2965e620
Abhay-0103 <ab0321054@gmail.com>
2025-08-08 23:38:23 +0530

    For Loop

---
+++ src/src/_4_Conditionals_and_Loops/_2_Loops.java
@@ -0,0 +1,21 @@
+package _4_Conditionals_and_Loops;
+
+public class _2_Loops {
+    public static void main(String[] args) {
+        // Q: Print number from 1 to 5
+
+        /*
+                syntax of For Loop
+                for (int ; condion; +/-) {
+                     // body
+                }
+         */
+
+        for (int num = 1; num <=5; num++ ){
+            System.out.println(num);
+        }
+
+
+
+    }
+}
