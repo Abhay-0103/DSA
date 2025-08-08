7af85490b1100b80b14c5fddc202c92b7dcb10a0
Abhay-0103 <ab0321054@gmail.com>
2025-08-08 23:36:51 +0530

    For Loop

--- src/src/_4_Conditionals_and_Loops/_1_if.java
+++ src/src/_4_Conditionals_and_Loops/_1_if.java
@@ -1,4 +1,37 @@
 package _4_Conditionals_and_Loops;
 
 public class _1_if {
-}
+
+        public static void main(String[] args) {
+
+            /* for if
+            if (boolrand epxopc t or f) {
+            // body
+            else {
+            // do this
+            }
+             */
+
+
+            int salary = 35400;
+//
+//            if (salary > 10000) {
+//                salary = salary + 2000;
+//            } else {
+//                salary = salary + 1000;
+//            }
+
+
+            // multi if-else
+
+            if (salary > 10000) {
+                salary += 2000;
+            } else if (salary > 20000) {
+                salary += 30000;
+            } else {
+                salary += 1000;
+            }
+
+            System.out.println("Final salary: " + salary);
+        }
+    }
