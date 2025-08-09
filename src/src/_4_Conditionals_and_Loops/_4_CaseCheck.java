657ee26253f3a9dc275bfd87c19f5d2aa0523896
Abhay-0103 <ab0321054@gmail.com>
2025-08-09 20:46:57 +0530

    Case-Check

---
+++ src/src/_4_Conditionals_and_Loops/_4_CaseCheck.java
@@ -0,0 +1,18 @@
+package _4_Conditionals_and_Loops;
+
+import java.util.Scanner;
+
+public class _4_CaseCheck {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().trim().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            System.out.println("Lowercase");
+        } else {
+            System.out.println("Uppercase");
+        }
+
+        System.out.println(ch);
+    }
+}
