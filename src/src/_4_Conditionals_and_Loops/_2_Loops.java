7c7ac20bf51b07e1710e5e48e6ff0eede6c5386d
Abhay-0103 <ab0321054@gmail.com>
2025-08-09 20:16:16 +0530

    Loops

--- src/src/_4_Conditionals_and_Loops/_2_Loops.java
+++ src/src/_4_Conditionals_and_Loops/_2_Loops.java
@@ -1,5 +1,7 @@
 package _4_Conditionals_and_Loops;
 
+import java.util.Scanner;
+
 public class _2_Loops {
     public static void main(String[] args) {
         // Q: Print number from 1 to 5
@@ -11,11 +13,53 @@ public class _2_Loops {
                 }
          */
 
-        for (int num = 1; num <=5; num++ ){
+
+
+        for (int num = 1; num <=5; num+= 1 ){
             System.out.println(num);
         }
 
+        // print number from 1 to n
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
 
+        for (int num = 1; num <=n ; num++) {
+           // System.out.println(num + " ");
+            System.out.println("Haa Bhai Kya Hal");
+        }
+
+        // while loops
+        /*
+        syntax:
+        while (condition ) {
+        // body
+        }
+         */
+
+
+        for (int num = 1; num <=5; num+= 1 ){
+            System.out.println(num);
+        }
+        int num = 1;
+        while (num <= 5) {
+           System.out.println(num);
+            num += 1;
+        }
+
+        // do while
+        /*
+
+            do {
+
+            } while (condition);
+
+         */
+
+            int n = 1;
+            do {
+                System.out.println(n);
+                n++;
+            } while (n <= 5);
 
     }
 }
