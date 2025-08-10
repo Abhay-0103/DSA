315188e8810b685c2b89bf504ca2065e7e549a5d
Abhay-0103 <ab0321054@gmail.com>
2025-08-10 19:48:52 +0530

    Nested

--- src/src/_5_Switch_and_Nested/_2_Nested.java
+++ src/src/_5_Switch_and_Nested/_2_Nested.java
@@ -1,4 +1,37 @@
 package _5_Switch_and_Nested;
 
+import java.util.Scanner;
+
 public class _2_Nested {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Emp ID : ");
+        int empID = in.nextInt();
+        System.out.println("Enter Department : ");
+        String department = in.next();
+
+        switch (empID) {
+            case 1:
+                System.out.println("Naam Hai Abhay");
+                break;
+            case 2:
+                System.out.println("Ka Ho Ka Hal Hai");
+                break;
+            case 3:
+                System.out.println("Emp Number 3");
+                switch (department) {
+                    case "IT":
+                        System.out.println("IT Department");
+                        break;
+                    case "Management":
+                        System.out.println("Management Department");
+                        break;
+                    default:
+                        System.out.println("No department enter");
+                }
+                break;
+            default:
+                System.out.println("Enter correct EmpID");
+        }
+    }
 }
