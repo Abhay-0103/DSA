f00274009e1391d57b25a0124d97bcd1d1c35d42
Abhay-0103 <ab0321054@gmail.com>
2025-08-12 17:38:40 +0530

    Armstong

--- src/src/_6_Functions_and_Methods/_7_Question.java
+++ src/src/_6_Functions_and_Methods/_7_Question.java
@@ -7,22 +7,44 @@ public class _7_Question {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        boolean ans = isPrime(n);
-        System.out.println(ans);
+//        boolean ans = isPrime(n);
+//        System.out.println(ans);
+
+        System.out.println(isArmstrong(n));
     }
 
-    static boolean isPrime(int n){
-        if (n <= 1) {
-            return false;
-        }
-        int c = 2;
-        while (c * c <= n ){
-            if (n % c == 0 ){
-                return false;
-            }
-            c++;
-        }
-        return c * c > n;
-    }
+    // print all the 3 digits armstrong number
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
 
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem * rem * rem;
+        }
+
+        return sum == original;
+    }
 }
+
+
+
+
+// for prime number
+
+//    static boolean isPrime(int n){
+//        if (n <= 1) {
+//            return false;
+//        }
+//        int c = 2;
+//        while (c * c <= n ){
+//            if (n % c == 0 ){
+//                return false;
+//            }
+//            c++;
+//        }
+//        return c * c > n;
+//    }
+//
+//}
