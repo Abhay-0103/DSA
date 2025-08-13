feb7ebbf985dde95e66387484688654fd89c34ae
Abhay-0103 <ab0321054@gmail.com>
2025-08-13 22:46:58 +0530

    Array

---
+++ src/src/_7_Array_and_ArrayList/_1_Array.java
@@ -0,0 +1,33 @@
+package _7_Array_and_ArrayList;
+
+public class _1_Array {
+    public static void main(String[] args) {
+        // Q :- Store a roll number
+        int a = 001;
+
+        // Q :- Store a person's name
+        String name = "Abhay Singh";
+
+        // Q :- Store 5 roll number
+        int rno1 = 002;
+        int rno2 = 023;
+        int rno3 = 012;
+        int rno4 = 032;
+        int rno5 = 045;
+
+        // Syntax
+
+        // datatype[] variable_name = new datatype[size];
+
+        // Storing 5 roll no
+        int[] roll1 = new int[5];
+        // or directly
+        int[] roll2 = {23, 24, 2, 23, 45};
+
+
+        int[] ros; // declarations of array, is gettng defined in the stack2
+        ros = new int[2]; // initialisation : its actualy here object is being creating ;
+
+
+    }
+}
