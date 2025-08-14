4d79d7901037f0323c8e6f5bca0135460b2228f6
Abhay-0103 <ab0321054@gmail.com>
2025-08-14 17:28:02 +0530

    Passing-In-Functions

---
+++ src/src/_7_Array_and_ArrayList/_3_PassingInFunctions.java
@@ -0,0 +1,16 @@
+package _7_Array_and_ArrayList;
+
+import java.util.Arrays;
+
+public class _3_PassingInFunctions {
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 12};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));
+
+    }
+    static void change(int[] arr){
+        arr[0] = 99;
+    }
+}
